9a9f208d7069d4193cbebd5da40717213ee85b68
Himanshu <xylogramori@gmail.com>
2023-05-22 21:28:02 +0530

    Download RESUME Added-4

--- src/download/Download.jsx
+++ src/download/Download.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './download.css'
-const PDF_URL="http://localhost:3000/RESUME_HIMANSHU.pdf"
+const PDF_URL="https://himanshu007-portfolio.netlify.app/RESUME_HIMANSHU.pdf"
 const Download = () => {
     const downloadFileAtUrl=(url)=>{
 fetch(url).then((response)=>response.blob()).then((blob)=>{
